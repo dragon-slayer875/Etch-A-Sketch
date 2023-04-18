61fc91752316bd68b32fd0acc0b1288f377eb86f
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2023-04-18 08:54:38 +0530

    Add JS with boilerplate function to modify tiles

---
+++ script.js
@@ -0,0 +1,10 @@
+const tile = document.createElement('div', )
+
+function modifyCanvas(numberOfTiles) {
+    for (let i = 0; i < numberOfTiles; i++) {
+        console.log('Hello')
+        // document.querySelector('.canvas').appendChild()    
+    }    
+}
+
+modifyCanvas(1)
