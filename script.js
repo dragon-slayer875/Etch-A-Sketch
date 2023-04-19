60757d08d711083cf3bf4a92e62fa3192610c21f
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2023-04-20 00:52:05 +0530

    Update index.html and script.js Added input elements on the webpage for taking inputs for canvas size, and updated the the modifyCanvas function to check if the value is larger than allowed (100).

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-const tile = document.createElement('div', )
+const canvasTile = document.createElement('div')
+canvasTile.className = 'tile';
 
 function modifyCanvas(numberOfTiles) {
+    if (numberOfTiles > 100) {
+        alert("Canvas too big! Enter a value less than 100.")
+        return;
+    }
     for (let i = 0; i < numberOfTiles; i++) {
-        console.log('Hello')
-        // document.querySelector('.canvas').appendChild()    
+        document.querySelector('.canvas').appendChild(canvasTile)    
     }    
 }
+
 
 modifyCanvas(1)
